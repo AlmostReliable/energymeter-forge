1a88027516ffab63eee8f01dead2ba5147ee750d
Relentless <relentless@rlnt.dev>
2021-10-10 09:59:11 +0200

    update entity renderer to support up and down facing

--- src/main/java/dev/rlnt/energymeter/client/MeterRenderer.java
+++ src/main/java/dev/rlnt/energymeter/client/MeterRenderer.java
@@ -20,7 +20,7 @@ public class MeterRenderer implements BlockEntityRenderer<MeterEntity> {
     private static final float[] ANGLE = { 0, 0, 0, 180, 90, -90 };
     private static final float PIXEL_SIZE = .3f / 16;
     private static final float OFFSET = 0.001f;
-    private static final int MAX_DISTANCE = 20;
+    private static final int MAX_DISTANCE = 30;
     private final Minecraft mc;
     private final Font font;
 
@@ -30,15 +30,15 @@ public class MeterRenderer implements BlockEntityRenderer<MeterEntity> {
     }
 
     private static Vector3f getFacingVector(Direction facing) {
-        final var y = .5f;
         if (facing.ordinal() < 2) {
-            throw new IllegalStateException("Facing can't be up or down!");
+            // up or down
+            return new Vector3f(.5f, facing == Direction.UP ? 1 + OFFSET : -OFFSET, .5f);
         } else if (facing.ordinal() < 4) {
             // north or south
-            return new Vector3f(.5f, y, facing == Direction.NORTH ? -OFFSET : 1 + OFFSET);
+            return new Vector3f(.5f, .5f, facing == Direction.NORTH ? -OFFSET : 1 + OFFSET);
         } else {
             // west or east
-            return new Vector3f(facing == Direction.WEST ? -OFFSET : 1 + OFFSET, y, .5f);
+            return new Vector3f(facing == Direction.WEST ? -OFFSET : 1 + OFFSET, .5f, .5f);
         }
     }
 
@@ -47,7 +47,7 @@ public class MeterRenderer implements BlockEntityRenderer<MeterEntity> {
     public void render(
         MeterEntity entity,
         float partial,
-        PoseStack matrix,
+        PoseStack stack,
         MultiBufferSource buffer,
         int light,
         int overlay
@@ -56,30 +56,54 @@ public class MeterRenderer implements BlockEntityRenderer<MeterEntity> {
         if (entity.getBlockPos().distSqr(mc.player.blockPosition()) > Math.pow(MAX_DISTANCE, 2)) return;
 
         // get the facing side and get the vector used for positioning
-        Direction facing = entity.getBlockState().getValue(MeterBlock.HORIZONTAL_FACING);
+        Direction facing = entity.getBlockState().getValue(MeterBlock.FACING);
+        Direction bottom = entity.getBlockState().getValue(MeterBlock.BOTTOM);
         Vector3f vector = getFacingVector(facing);
 
-        matrix.pushPose();
+        stack.pushPose();
         // move and rotate the position according to the facing
-        matrix.translate(vector.x(), vector.y(), vector.z());
-        matrix.mulPose(new Quaternion(0, ANGLE[facing.ordinal()], 180, true));
+        stack.translate(vector.x(), vector.y(), vector.z());
+        /*
+           The rotation of the matrix depends on the facing direction of the block and
+           where the screen is located.
+           The calculations are different if the facing direction is up or down.
+           By default, a Quaternion consists of the three axis x, y and z.
+           When opening the F3 debug screen, the coloring of the axis is the following:
+           x = red, y = green, z = blue
+           When using a Quaternion, rotations are always applied in the order z, y, x.
+           At this point, we need to keep in mind that the axis are also rotated.
+           Example:
+           When we rotate 90 degrees around z (blue axis), the red axis (x) becomes
+           the green axis (y).
+         */
+        if (facing != bottom) {
+            stack.mulPose(Vector3f.ZN.rotationDegrees(180));
+            stack.mulPose(Vector3f.XN.rotationDegrees(facing == Direction.UP ? 90 : -90));
+            stack.mulPose(
+                Vector3f.ZN.rotationDegrees(
+                    facing == Direction.DOWN ? (180 - ANGLE[bottom.ordinal()]) : ANGLE[bottom.ordinal()]
+                )
+            );
+        } else {
+            stack.mulPose(new Quaternion(0, ANGLE[facing.ordinal()], 180, true));
+        }
         // scale the matrix so the text fits on the screen
-        matrix.scale(PIXEL_SIZE, PIXEL_SIZE, 0);
+        stack.scale(PIXEL_SIZE, PIXEL_SIZE, 0);
         // format the current flow rate and draw it according to its size, so it's centered
         Tuple<String, String> text = TextUtils.formatEnergy(entity.getTransferRate(), false);
         String flowRate = text.getA();
         String unit = text.getB() + "/t";
         // flow rate
         font.draw(
-            matrix,
+            stack,
             flowRate,
             font.width(flowRate) / -2f,
             -font.lineHeight - OFFSET,
             ChatFormatting.WHITE.getColor()
         );
         // unit
-        font.draw(matrix, unit, font.width(unit) / -2f, OFFSET, ChatFormatting.WHITE.getColor());
+        font.draw(stack, unit, font.width(unit) / -2f, OFFSET, ChatFormatting.WHITE.getColor());
 
-        matrix.popPose();
+        stack.popPose();
     }
 }
