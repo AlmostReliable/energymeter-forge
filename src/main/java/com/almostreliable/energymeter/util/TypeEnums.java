1d9b046ddf855980cabe43dd25bb8607d510365e
rlnt <relentless@rlnt.dev>
2025-04-30 11:39:54 +0200

    remove unused enums

--- src/main/java/com/almostreliable/energymeter/util/TypeEnums.java
+++ src/main/java/com/almostreliable/energymeter/util/TypeEnums.java
@@ -4,30 +4,10 @@ public final class TypeEnums {
 
     private TypeEnums() {}
 
-    /**
-     * Enum to represent the type of the translation to identify its key inside the lang file.
-     */
-    public enum TranslateType {
-        CONTAINER, LABEL, TOOLTIP, BLOCK_SIDE, IO_SETTING, NUMBER, STATUS, MODE, ACCURACY
-    }
-
-    /**
-     * Enum to represent a setting for the different GUI buttons.
-     */
-    public enum Setting {
-        NUMBER, MODE, ACCURACY
-    }
-
-    /**
-     * Enum to represent the status of the meter.
-     */
     public enum ConnectionStatus {
         DISCONNECTED, IDLE, SPLITTING, TRANSFERRING, CONSUMING
     }
 
-    /**
-     * Enum to represent the mode of the meter.
-     */
     public enum TransferMode implements EnumExtension {
 
         SPLIT(true, true),
@@ -51,17 +31,7 @@ public final class TypeEnums {
         }
     }
 
-    /**
-     * Enum to represent the calculation mode of the meter.
-     */
     public enum MeasureMode {
         EXACT, INTERVAL
-    }
-
-    /**
-     * Enum to represent the different types of text boxes.
-     */
-    public enum TextBox {
-        INTERVAL, THRESHOLD
     }
 }
