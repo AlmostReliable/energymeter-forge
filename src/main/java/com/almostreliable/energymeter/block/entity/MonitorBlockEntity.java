49fbd908c4f25c6c2cbc5fee432847878046b507
rlnt <relentless@rlnt.dev>
2026-02-25 10:35:45 +0100

    remove redundant menu provider interface from block entity

--- src/main/java/com/almostreliable/energymeter/block/entity/MonitorBlockEntity.java
+++ src/main/java/com/almostreliable/energymeter/block/entity/MonitorBlockEntity.java
@@ -8,7 +8,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -17,7 +16,7 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import org.jetbrains.annotations.Nullable;
 
-public class MonitorBlockEntity extends BlockEntity implements TickableMenuBlockEntity, MenuProvider {
+public class MonitorBlockEntity extends BlockEntity implements TickableMenuBlockEntity {
 
     private int width;
     private int height;
