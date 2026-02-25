1e8cb9b42d789b199165cf900ef6d77c3df5f987
rlnt <relentless@rlnt.dev>
2026-02-25 10:23:56 +0100

    add wip text to monitor tooltip

--- src/main/java/com/almostreliable/energymeter/block/MonitorBlock.java
+++ src/main/java/com/almostreliable/energymeter/block/MonitorBlock.java
@@ -14,6 +14,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -26,6 +29,7 @@ import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MonitorBlock extends FacingEntityBlock {
@@ -248,6 +252,12 @@ public class MonitorBlock extends FacingEntityBlock {
         return state
             .setValue(HORIZONTAL, OptionalDirection.fromDirection(nearestDirection))
             .setValue(VERTICAL, controllerPos.getY() < pos.getY() ? OptionalDirection.DOWN : OptionalDirection.NONE);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        tooltipComponents.add(Component.literal("Work in progress!").withStyle(ChatFormatting.DARK_RED));
     }
 
     private record MonitorSearchResult(int distance, BlockPos pos) {}
