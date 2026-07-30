a3531cbd49cb0c396e760e32553cd48cf6dff48f
Saereth <paulanthonydevs@gmail.com>
2026-07-29 22:14:53 -0700

    port blocks and block states
    Note, Block#appendHoverText is gone, so I moved the monitor's tooltip to block item

--- src/main/java/com/almostreliable/energymeter/block/FacingEntityBlock.java
+++ src/main/java/com/almostreliable/energymeter/block/FacingEntityBlock.java
@@ -9,14 +9,14 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 
 import org.jetbrains.annotations.Nullable;
 
 public abstract class FacingEntityBlock extends TickableMenuBlock {
 
-    public static final DirectionProperty FACING = BlockStateProperties.FACING;
-    public static final DirectionProperty BOTTOM = DirectionProperty.create(Constants.BOTTOM_PROP);
+    public static final EnumProperty<Direction> FACING = BlockStateProperties.FACING;
+    public static final EnumProperty<Direction> BOTTOM = EnumProperty.create(Constants.BOTTOM_PROP, Direction.class);
 
     FacingEntityBlock(Properties properties) {
         super(properties);
