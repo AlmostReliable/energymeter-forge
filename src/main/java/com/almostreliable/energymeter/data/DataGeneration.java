5d29d2e5a687c4418f5c43d22278994b9fa2e3ad
Saereth <paulanthonydevs@gmail.com>
2026-07-29 22:33:40 -0700

    ported the datagen to the vanilla model providers

--- src/main/java/com/almostreliable/energymeter/data/DataGeneration.java
+++ src/main/java/com/almostreliable/energymeter/data/DataGeneration.java
@@ -2,24 +2,30 @@ package com.almostreliable.energymeter.data;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 public final class DataGeneration {
 
     private DataGeneration() {}
 
-    public static void init(GatherDataEvent event) {
+    public static void initClient(GatherDataEvent.Client event) {
+        init(event);
+        DataGenerator generator = event.getGenerator();
+        generator.addProvider(true, new EnergyMeterModels(generator.getPackOutput()));
+    }
+
+    public static void initServer(GatherDataEvent.Server event) {
+        init(event);
+    }
+
+    private static void init(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
-        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-        var registryAccess = event.getLookupProvider();
+        var registries = event.getLookupProvider();
 
-        generator.addProvider(event.includeClient(), new EnergyMeterLang(output));
-        generator.addProvider(event.includeClient(), new EnergyMeterModels(output, existingFileHelper));
-
-        generator.addProvider(event.includeServer(), new EnergyMeterLoot(output, registryAccess));
-        generator.addProvider(event.includeServer(), new EnergyMeterRecipes(output, registryAccess));
-        generator.addProvider(event.includeServer(), new EnergyMeterTags(output, registryAccess, existingFileHelper));
+        generator.addProvider(true, new EnergyMeterLang(output));
+        generator.addProvider(true, new EnergyMeterLoot(output, registries));
+        generator.addProvider(true, new EnergyMeterRecipes.Runner(output, registries));
+        generator.addProvider(true, new EnergyMeterTags(output, registries));
     }
 }
