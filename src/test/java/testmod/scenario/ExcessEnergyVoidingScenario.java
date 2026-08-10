564e2c0ebb1986e9f343f93953d1a617838bd3a5
rlnt <16513358+rlnt@users.noreply.github.com>
2026-08-10 17:41:03 +0200

    fix javadoc link in excess voiding scenario

--- src/test/java/testmod/scenario/ExcessEnergyVoidingScenario.java
+++ src/test/java/testmod/scenario/ExcessEnergyVoidingScenario.java
@@ -5,6 +5,8 @@ import com.almostreliable.energymeter.block.entity.MeterBlockEntity;
 
 import net.minecraft.gametest.framework.GameTestHelper;
 
+import net.neoforged.neoforge.transfer.transaction.TransactionContext;
+
 import testmod.TestMod;
 import testmod.TestUtils;
 import testmod.TestUtils.SimplePlotResult;
@@ -16,7 +18,7 @@ import testmod.content.EnergyReceiverBlockEntity;
  * When sending energy to a block that only accepts less than its designated amount, the
  * excess energy is voided instead of being returned.
  * <p>
- * This was fixed by returning the forwarded energy in {@link MeterEnergyHandler#forwardEnergy(int, boolean)}.
+ * This was fixed by returning the forwarded energy in {@link MeterEnergyHandler#forwardEnergy(int, TransactionContext)}.
  */
 public class ExcessEnergyVoidingScenario {
 
